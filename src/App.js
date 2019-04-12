8b70d06e2c9a92ddffaedb24636033ad446f1a73
Jesus Rojas <jesus@lacafetalab.pe>
2019-04-12 10:57:53 -0500

    Initial commit on repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      idUsuario: '',
+      dataUsuario: {}
+    }
+    this.handlerChange = this.handlerChange.bind(this)
+  }
+
+  componentDidMount(){
+    this.handlerFetchFunction()
+  }
+
+  handlerFetchFunction(){ 
+    const url = `https://jsonplaceholder.typicode.com/todos/${this.state.idUsuario}`
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      }).then(function (resultado) {
+        this.setState({
+          dataUsuario: resultado
+        })
+      }.bind(this))
+      .catch(error => {
+        alert('error', error);
+      });
+  }
+
+  handlerChange(event){
+    const { value } = event.target;
+    this.setState({
+      idUsuario: value
+    }, () => this.handlerFetchFunction());
+  }
+
+
   render() {
+    const { dataUsuario } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input
+          name="idUsuario"
+          value={this.state.idUsuario}
+          onChange={this.handlerChange}
+          type="number"
+        />
+        <div className="table-wraper">
+          { dataUsuario ? 
+            <table>
+              <thead>
+                <tr>
+                  <th>id</th>
+                  <th>user id</th>
+                  <th>title</th>
+                  <th>status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{dataUsuario.id}</td>
+                  <td>{dataUsuario.userId}</td>
+                  <td>{dataUsuario.title}</td>
+                  <td>{dataUsuario.completed}</td>
+                </tr>
+              </tbody>
+            </table>
+            :
+            <p>No se encontraron coincidencias.</p>
+          }
+        </div>
       </div>
     );
   }
